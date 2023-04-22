b8ee7b283d61787c32d49ab2d1f736a873fa4ffc
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2023-04-22 07:05:53 +0000

    feat: receiving connections

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello, world!');
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+	/* options */
+});
+
+io.on('connection', socket => {
+	console.log(socket.id);
+	io.emit('response', 'valeu');
+
+	socket.on('message', message => {
+		console.log(message);
+	});
+});
+
+httpServer.listen(4521);
