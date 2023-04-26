762de5a0276d4ebf38442f016879ed0a8b29f6ca
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2023-04-26 04:03:08 +0000

    feat: creating command parser with arcsecond

---
+++ src/Parser/index.ts
@@ -0,0 +1,96 @@
+import {
+	anyCharExcept,
+	sequenceOf,
+	many,
+	many1,
+	char,
+	choice,
+	str,
+	possibly,
+	Parser,
+} from 'arcsecond';
+
+//Utilities
+const space = char(' ');
+const anythingBut = (s: string) => many1(anyCharExcept(str(s))).map(r => r.join(''));
+const anythingButSpace = anythingBut(' ');
+const anythingButParser = <T, D extends string, E>(p: Parser<T, D, E>) =>
+	many1(anyCharExcept(p)).map(r => r.join(''));
+
+//Pieces of command
+const commandChar = choice([char('!'), char('/')]);
+
+const modName = sequenceOf([many(space), anythingButSpace]).map(r => r[1]);
+
+const method = possibly(
+	sequenceOf([many(space), anythingButSpace]).map(r => r[1])
+).map(x => x || 'default');
+
+const immediateArg = sequenceOf([many(space), anythingBut('--')]).map(r =>
+	r[1].trim()
+);
+
+/**
+ * I thought it would be better to use this as an IIFE
+ * closing `argName` and `argValue` inside the scope of
+ * the namedArgs definition
+ */
+const namedArgs = (() => {
+	const argName = sequenceOf([many(space), str('--'), anythingButSpace]).map(
+		r => r[2]
+	);
+
+	const argValue = possibly(
+		sequenceOf([
+			many1(space),
+			anythingButParser(choice([char(' '), char('-')])),
+		]).map(x => x[1])
+	).map(x => x || 'true');
+
+	return many1(
+		sequenceOf([argName, argValue]).map(r => ({
+			[r[0]]: r[1],
+		}))
+	).map(x =>
+		x.reduce((args, currArg) => {
+			return {
+				...args,
+				...currArg,
+			};
+		})
+	);
+})();
+
+const parser = sequenceOf([
+	commandChar,
+	modName,
+	method,
+	possibly(immediateArg),
+	possibly(namedArgs),
+]).map(r => ({
+	module: r[1],
+	method: r[2],
+	immediateArg: r[3],
+	namedArgs: r[4],
+}));
+
+/**
+ * Utility parsing function that adds more data for the user
+ * to debug where the parsing failed
+ * @param s
+ * @returns
+ */
+export const parse = (s: string) => {
+	const result = parser.run(s);
+	if (result.isError) {
+		return {
+			...result,
+			stringLeft: s.slice(result.index - 1, result.index + 20) + '...',
+		};
+	}
+
+	return {
+		...result,
+		stringLeft: '',
+	};
+};
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { Server } from 'socket.io';
 const io = new Server(4521, {});
 import { registerHandlers } from './Events/Handling';
+import { parse } from './Parser';
 
 io.on('connection', socket => {
 	console.log(socket.id);
 
 	registerHandlers(socket);
 });
+
+const string =
+	'/yt download https://yt.com/video?v=ajk2c --this-is --but-that-is false';
+
+console.log(JSON.stringify(parse(string), undefined, '  '));
