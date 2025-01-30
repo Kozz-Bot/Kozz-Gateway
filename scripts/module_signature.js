796d45836a0fbefc651479c69fa2ea59f15dcbc4
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2025-01-30 14:16:11 -0300

    fix iwac introduction payload again

--- scripts/module_signature.js
+++ scripts/module_signature.js
@@ -33,7 +33,6 @@ const signature = crypto.sign(
 	{ key: privateKey, padding: crypto.constants.RSA_PKCS1_PSS_PADDING }
 );
 
-console.log('This is the payload you have to paste on postman');
 console.log({
 	...payload,
 	signature: signature.toString('base64'),
